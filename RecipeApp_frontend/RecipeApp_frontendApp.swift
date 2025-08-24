4507392ec82b13f319d2d9379d0c8a215c73270d
tingqu <quting3@gmail.com>
2025-08-24 00:54:07 -0400

    Initial Commit

---
+++ RecipeApp_frontend/RecipeApp_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp_frontendApp.swift
+//  RecipeApp_frontend
+//
+//  Created by TingQu on 8/24/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeApp_frontendTests/RecipeApp_frontendTests.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeApp_frontendTests.swift
+//  RecipeApp_frontendTests
+//
+//  Created by TingQu on 8/24/25.
+//
+
+import Testing
+
+struct RecipeApp_frontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
